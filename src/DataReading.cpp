121459d9a2ba3e405dd6806861615e65225dd277
Sukhbir Mahal <sukhim2004@outlook.com>
2024-10-25 18:56:47 +0100

    preliminary code for pressure readings

--- src/DataReading.cpp
+++ src/DataReading.cpp
@@ -1,6 +1,31 @@
 #include <DataReading.h>
 
+
 DataReading::DataReading()
 {
+  if (!ps.init())
+  {
+    Serial.println("Failed to autodetect pressure sensor!");
+    while (1);
+  }
+
+  ps.enableDefault();
+
+  altitudeCalibration();
+
+  Serial.println("Calibration complete");
     
+}
+
+void DataReading::altitudeCalibration()
+{
+    float altitudeSum = 0;
+
+  for (altIter = 0; altIter < ITER_NO; altIter++){
+    pressure = ps.readPressureMillibars();
+    altitude = ps.pressureToAltitudeMeters(pressure);
+    altitudeSum += altitude;
+  }
+
+  altitudeOffset = altitudeSum / ITER_NO;
 }
--- src/DataReading.h
+++ src/DataReading.h
@@ -1,8 +1,16 @@
+#include <Arduino.h>
+#include <LPS.h>
+
 class DataReading
 {
 private:
     /* data */
+    LPS ps;
+    const int ITER_NO = 1000;
+    int altIter;
+    float pressure = 0.0, altitude = 0.0, temperature = 0.0, altitudeOffset = 0.0;
 public:
-    DataReading(/* args */);
+    DataReading(/* args */); 
+    void altitudeCalibration();
 };
 
