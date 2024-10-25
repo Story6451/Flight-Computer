ae3af1bd9b751185e1d9961eb65df27f9ce2f266
Sukhbir Mahal <sukhim2004@outlook.com>
2024-10-25 19:11:07 +0100

    a

--- src/DataReading.h
+++ src/DataReading.h
@@ -1,14 +1,10 @@
 #include <Arduino.h>
-<<<<<<< HEAD
 #include <LPS.h>
-=======
->>>>>>> 361a33b705f11f4376cb86d935df70aad5e24ffd
 
 class DataReading
 {
 private:
     /* data */
-<<<<<<< HEAD
     LPS ps;
     const int ITER_NO = 1000;
     int altIter;
@@ -16,14 +12,6 @@ private:
 public:
     DataReading(/* args */); 
     void altitudeCalibration();
-=======
-    /*Magnetometer reading in gauss 1T = 1E4 GAUSS*/
-    double MagnetometerXValue;
-    double MagnetometerYValue;
-    double MagnetometerZValue;
-public:
-    DataReading(/* args */);
-    void ReadMagnetometer();
->>>>>>> 361a33b705f11f4376cb86d935df70aad5e24ffd
+    void printAltitude();
 };
 
