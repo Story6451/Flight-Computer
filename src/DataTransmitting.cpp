544c236eab7d50b9935a88adeac02d27ea5bca81
Budgingcobra <Prathitbhat6@gmail.com>
2024-11-06 13:39:28 +0000

    Made the parts into functions on the .h file

--- src/DataTransmitting.cpp
+++ src/DataTransmitting.cpp
@@ -10,64 +10,42 @@ DataTransmitting::DataTransmitting()
     LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
 
     // (list of all data to recieve and in which format)
-    //uint32_t pressure, uint16_t temperature, std::vector<int16_t> acceleration, uint32_t magneticFluxDensity, std::vector<int16_t> rotation, std::vector<int16_t> gpsCoordinates, int16_t velocity/100, uint16_t altitude 
-    // dummy data for testing the rest of the code
-    uint32_t pressure = 10000;
-    uint16_t temperature = 100;
-    std::vector<int16_t> acceleration = { 2, -1, 4 };
-    uint32_t magneticFluxDensity = 100;
-    std::vector<int16_t> rotation = { 0, 1, -4 };
-    std::vector<int16_t> gpsCoordinates = { 200, 100 };
-    int16_t velocity = -20;
-    uint16_t altitude = 2000;
+    //uint32_t pressure, uint16_t temperature, std::vector<int16_t> acceleration, 
+    //uint32_t magneticFluxDensity, std::vector<int16_t> rotation, std::vector<int16_t> gpsCoordinates,
+    //int16_t velocity/100, uint16_t altitude 
      
     // creating the packet and the start byte
-    const uint8_t START_BYTE = 0xAA;
-    std::vector<uint8_t> packet = { START_BYTE };
+    std::vector<uint8_t> packet = CreatePacket(0xAA);
 
     // breaking apart and adding all of the data to the packet
-    packet.push_back((pressure >> 24) & 0xFF);
-    packet.push_back((pressure >> 16) & 0xFF);
-    packet.push_back((pressure >> 8) & 0xFF);
-    packet.push_back(pressure & 0xFF);
-
-    packet.push_back((temperature >> 8) & 0xFF);
-    packet.push_back(temperature & 0xFF);
+    Parse32Bit(packet, pressure);
+    Parse16Bit(packet, temperature); 
 
     for (int16_t value : acceleration){
-        packet.push_back((value >> 8) & 0xFF);
-        packet.push_back(value & 0xFF);
+        Parse16Bit(packet, value);
     }
 
-    packet.push_back((magneticFluxDensity >> 24) & 0xFF);
-    packet.push_back((magneticFluxDensity >> 16) & 0xFF);
-    packet.push_back((magneticFluxDensity >> 8) & 0xFF);
-    packet.push_back(magneticFluxDensity & 0xFF);
+    Parse32Bit(packet, magneticFluxDensity);
 
     for (int16_t value : rotation){
-        packet.push_back((value >> 8) & 0xFF);
-        packet.push_back(value & 0xFF);
+        Parse16Bit(packet, value);
     }
 
     for (int16_t gpsCoordinate : gpsCoordinates){
-        packet.push_back((gpsCoordinate >> 8) & 0xFF);
-        packet.push_back(gpsCoordinate & 0xFF);
+        Parse16Bit(packet, gpsCoordinate);
     }
 
-    packet.push_back((velocity >> 8) & 0xFF);
-    packet.push_back(velocity & 0xFF);
-
-    packet.push_back((altitude >> 8) & 0xFF);
-    packet.push_back(altitude & 0xFF);
+    Parse16Bit(packet, velocity);
+    Parse16Bit(packet, altitude);
 
     // calculating and adding the checksum to the packet
     uint16_t checksum = 0;
     for (size_t i=1; i<packet.size(); i++){
         checksum ^= packet[i];
     }
+    packet.push_back(checksum);
 
     // for UI tests
-    packet.push_back(checksum);
     for (uint8_t value : packet){
         Serial.println(value);
     }
--- src/DataTransmitting.h
+++ src/DataTransmitting.h
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-
+#include <vector>
+#include <cstdint>
 
 class DataTransmitting
 {
@@ -8,7 +9,37 @@ private:
     const int resetPin = 4;       // LoRa radio reset
     const int irqPin = 2;         // Change for your board; must be a hardware interrupt pin
     /* data */
+    // dummy data for testing the rest of the code
+    uint32_t pressure = 10000;
+    uint16_t temperature = 100;
+    std::vector<int16_t> acceleration = { 2, -1, 4 };
+    uint32_t magneticFluxDensity = 100;
+    std::vector<int16_t> rotation = { 0, 1, -4 };
+    std::vector<int16_t> gpsCoordinates = { 200, 100 };
+    int16_t velocity = -2;
+    uint16_t altitude = 2000;
 public:
+    // creating the packet and adds the start byte to it
+    std::vector<uint8_t> CreatePacket(uint8_t start_byte){
+        const uint8_t START_BYTE = start_byte;
+        std::vector<uint8_t> packet = { START_BYTE };
+        return packet;
+    }
+
+    // breaks apart any 32bit data into 8bit chunks and adds it to the packet
+    void Parse32Bit(std::vector<uint8_t> packet, uint32_t data){
+        packet.push_back((data >> 24) & 0xFF);
+        packet.push_back((data >> 16) & 0xFF);
+        packet.push_back((data >> 8) & 0xFF);
+        packet.push_back(data & 0xFF);
+    }
+
+    // breaks apart any 16bit data into 8bit chunks and adds it to the packet
+    void Parse16Bit(std::vector<uint8_t> packet, uint16_t data){
+        packet.push_back((temperature >> 8) & 0xFF);
+        packet.push_back(temperature & 0xFF);
+    }
+
     DataTransmitting();
     //uint32_t pressure, uint16_t temperature, std::vector<int16_t> acceleration, uint32_t magneticFluxDensity, std::vector<int16_t> rotation, std::vector<int16_t> gpsCoordinates, int16_t velocity/100, uint16_t altitude 
 };
