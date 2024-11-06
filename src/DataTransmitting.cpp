c59170d7acd6181b5676bc25c31f12ea4e87df8e
Budgingcobra <Prathitbhat6@gmail.com>
2024-11-06 13:01:17 +0000

    Adding the packet structure that ground station wants

--- src/DataTransmitting.cpp
+++ src/DataTransmitting.cpp
@@ -1,10 +1,17 @@
 #include <DataTransmitting.h>
+#include <vector>
+#include <cstdint>
+#define LORA		//Specify that module will be used for LoRa to LoRa communication
+#include <SPI.h>
+#include "LoRa-SOLDERED.h"
 
 DataTransmitting::DataTransmitting()
 {
+    LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
     // (list of all data to recieve and in which format)
     //uint32_t pressure, uint16_t temperature, std::vector<int16_t> acceleration, uint32_t magneticFluxDensity, std::vector<int16_t> rotation, std::vector<int16_t> gpsCoordinates, int16_t velocity/100, uint16_t altitude 
-    // dummy data for the rest of the code to work
+    // dummy data for testing the rest of the code
     uint32_t pressure = 10000;
     uint16_t temperature = 100;
     std::vector<int16_t> acceleration = { 2, -1, 4 };
@@ -54,9 +61,22 @@ DataTransmitting::DataTransmitting()
     packet.push_back(altitude & 0xFF);
 
     // calculating and adding the checksum to the packet
-    uint8_t checksum = 0;
+    uint16_t checksum = 0;
     for (size_t i=1; i<packet.size(); i++){
         checksum ^= packet[i];
     }
+
+    // for UI tests
     packet.push_back(checksum);
+    for (uint8_t value : packet){
+        Serial.println(value);
+    }
+
+    // write to LoRa
+    // Send packet
+    LoRa.beginPacket(); //Begin sending
+    for (uint8_t value : packet){
+        LoRa.print(value);
+    }
+    LoRa.endPacket(); //End sending
 };
--- src/DataTransmitting.h
+++ src/DataTransmitting.h
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
+
 class DataTransmitting
 {
 private:
+    const int csPin = 10;          // LoRa radio chip select
+    const int resetPin = 4;       // LoRa radio reset
+    const int irqPin = 2;         // Change for your board; must be a hardware interrupt pin
     /* data */
 public:
-    DataTransmitting(/*args*/);
+    DataTransmitting();
+    //uint32_t pressure, uint16_t temperature, std::vector<int16_t> acceleration, uint32_t magneticFluxDensity, std::vector<int16_t> rotation, std::vector<int16_t> gpsCoordinates, int16_t velocity/100, uint16_t altitude 
 };
 
