f52418060f102e22dc558572e7885c27caff60e7
Budgingcobra <Prathitbhat6@gmail.com>
2024-11-08 19:09:55 +0000

    Updating DataReading with Adi's code

--- src/DataReading.cpp
+++ src/DataReading.cpp
@@ -51,43 +51,60 @@ void DataReading::AltitudeCalibration()
   altitudeOffset = altitudeSum / ITER_NO;
 }
 
-void DataReading::ReadAccelerometer()
-{
-  AccelXValue = imu.a.x;
-  AccelYValue = imu.a.y;
-  AccelZValue = imu.a.z;
-}
-void DataReading::ReadMagnetometer()
-{
-  mag.read();
-  MagnetometerXValue = mag.m.x;
-  MagnetometerYValue = mag.m.y;
-  MagnetometerZValue = mag.m.z;
-  // Output raw x, y, z values for magnetometer
-  Serial.println("MAGNETOMETER READINGS");
-  Serial.print("Magnetometer: X=");
-  Serial.print(MagnetometerXValue);
-  Serial.print(" Y=");
-  Serial.print(MagnetometerYValue);
-  Serial.print(" Z=");
-  Serial.println(MagnetometerZValue);
+float AccelXValue, AccelYValue, AccelZValue;
+float MagnetometerXValue, MagnetometerYValue, MagnetometerZValue;
+float MagnetometerPitch, MagnetometerRoll;
+float MagnetometerXCalculated, MagnetometerYCalculated;
+float UncalibratedHeading;
+
+void ReadAccelerometer() {
+    AccelXValue = imu.a.x;
+    AccelYValue = imu.a.y;
+    AccelZValue = imu.a.z;
 }
 
-void DataReading::CalculateHeading()
-{
-  MagnetometerPitch = atan(AccelXValue / sqrt(pow(AccelYValue,2)+pow(AccelZValue,2)));
-  MagnetometerRoll = atan(-AccelYValue / AccelZValue);
-  MagnetometerXCalculated = (MagnetometerXValue * cos(MagnetometerPitch)) + (MagnetometerZValue * sin(MagnetometerPitch));
-  MagnetometerYCalculated = ((MagnetometerXValue * sin(MagnetometerPitch) * sin(MagnetometerRoll)) + (MagnetometerYValue * cos(MagnetometerRoll)) - (MagnetometerZValue * sin(MagnetometerRoll) * cos(MagnetometerPitch)));
-  UncalibratedHeading = atan(MagnetometerYCalculated / MagnetometerXCalculated);
-  Serial.print("Uncalibrated Heading:");
-  Serial.println(UncalibratedHeading);
+void ReadMagnetometer() {
+    mag.read();
+    MagnetometerXValue = mag.m.x;
+    MagnetometerYValue = mag.m.y;
+    MagnetometerZValue = mag.m.z;
+
+    Serial.println("MAGNETOMETER READINGS");
+    Serial.print("Magnetometer: X=");
+    Serial.print(MagnetometerXValue);
+    Serial.print(" Y=");
+    Serial.print(MagnetometerYValue);
+    Serial.print(" Z=");
+    Serial.println(MagnetometerZValue);
 }
 
-void DataReading::ReadBarometer()
-{
-  temperature = ps.readTemperatureC();
-  pressure = ps.readPressureMillibars();
-  altitude = ps.pressureToAltitudeMeters(pressure);
+void CalculateHeading() {
+    float normAcc = sqrt(AccelXValue * AccelXValue + AccelYValue * AccelYValue + AccelZValue * AccelZValue);
+    AccelXValue /= normAcc;
+    AccelYValue /= normAcc;
+    AccelZValue /= normAcc;
+
+    MagnetometerPitch = asin(-AccelXValue);
+    MagnetometerRoll = asin(AccelYValue / cos(MagnetometerPitch));
+
+    MagnetometerXCalculated = (MagnetometerXValue * cos(MagnetometerPitch)) + (MagnetometerZValue * sin(MagnetometerPitch));
+    MagnetometerYCalculated = (MagnetometerXValue * sin(MagnetometerRoll) * sin(MagnetometerPitch)) + (MagnetometerYValue * cos(MagnetometerRoll)) - (MagnetometerZValue * sin(MagnetometerRoll) * cos(MagnetometerPitch));
+
+    UncalibratedHeading = atan2(MagnetometerYCalculated, MagnetometerXCalculated);
+
+    UncalibratedHeading = UncalibratedHeading * 180 / M_PI;
+    if (UncalibratedHeading < 0) {
+        UncalibratedHeading += 360;
+    }
+
+    Serial.print("Uncalibrated Heading:");
+    Serial.println(UncalibratedHeading);
 }
-    
+
+int main() {
+    ReadAccelerometer();
+    ReadMagnetometer();
+    CalculateHeading();
+
+    return 0;
+}
